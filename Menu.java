e1bee3ffc6b548732a97c53d8242ebc4e9d8648f
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2025-11-04 18:37:48 +0530

    Finished phase 2 1. added a password text file 2. gave master key updating for first time and resetting it

--- Menu.java
+++ Menu.java
@@ -1,12 +1,34 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.util.*;
 
 public class Menu {
+
+    //Reset method 
+    public static void reset(Scanner sc) throws Exception {
+        System.out.println("Enter the new password: ");
+        String input = sc.nextLine();
+        FileWriter fw = new FileWriter("master.txt",false);
+        fw.write(input);
+        fw.close();
+        System.out.println("Master password reset successfully!");
+    }
+
     // Master Password 
     public static boolean check(Scanner sc) throws Exception {
+        File master = new File("master.txt");
+        if(!master.exists()) {
+            System.out.println("No master password found.Set up a new password: ");
+            String newpass = sc.nextLine();
+            java.io.FileWriter fw = new java.io.FileWriter(master);
+            fw.write(newpass);
+            fw.close();
+            System.out.println("Master password created succesfully!!");
+            return true;
+        }
         System.out.println("Enter the Master Password: ");
         String input = sc.nextLine();
-        Scanner fileScanner = new Scanner(new File("master.txt"));
+        Scanner fileScanner = new Scanner(master);
         String saved = fileScanner.nextLine();
         fileScanner.close();
         if(input.equals(saved)) {
@@ -15,6 +37,11 @@ public class Menu {
         }
         else {
             System.out.println("Access denied");
+            System.out.println("Forgetten master password? (Y/N): ");
+            String in = sc.nextLine();
+            if(in.equals("Y")) {
+                reset(sc);
+            }
             return false;
         }
     }
@@ -30,6 +57,37 @@ public class Menu {
         public String toString() {
             return "Username: "+username+"\nPassword: "+password;
         }
+    }
+
+    //Saving details into file
+    public static void save(HashMap<String,Credential> pwds) throws Exception {
+        FileWriter fw = new FileWriter("data.txt");
+        for(String acc : pwds.keySet()) {
+            Credential c = pwds.get(acc);
+            fw.write(acc+","+c.username+","+c.password+"\n");
+        }
+        fw.close();
+    }
+
+    //Get details from file
+    public static void load(HashMap<String,Credential> pwds) throws Exception {
+        File f = new File("data.txt");
+        if(!f.exists()) {
+            System.out.println("No details have been saved!!");
+            return;
+        }
+        Scanner filScanner = new Scanner(f);
+        while(filScanner.hasNextLine()) {
+            String curr = filScanner.nextLine();
+            String[] sets = curr.split(",");
+            if(sets.length == 3) {
+                String acc = sets[0];
+                String usr = sets[1];
+                String pass = sets[2];
+                pwds.put(acc, new Credential(usr, pass));
+            }
+        }
+        filScanner.close();
     }
 
     //Add method
@@ -84,13 +142,18 @@ public class Menu {
     public static void update(HashMap<String,Credential> pwds,Scanner sc) {
         System.out.println("Enter the account whose details are to be updated: ");
         String ud = sc.nextLine();
-        System.out.println("Enter the new username: ");
-        String nusr = sc.nextLine();
-        System.out.println("Enter the new password: ");
-        String npass = sc.nextLine();
-        Credential ncreds = new Credential(nusr, npass);
-        pwds.put(ud,ncreds);
-        System.out.println("The details have been updated!");
+        if(pwds.containsKey(ud)) {
+            System.out.println("Enter the new username: ");
+            String nusr = sc.nextLine();
+            System.out.println("Enter the new password: ");
+            String npass = sc.nextLine();
+            Credential ncreds = new Credential(nusr, npass);
+            pwds.put(ud,ncreds);
+            System.out.println("The details have been updated!");
+        }
+        else {
+            System.out.println("No details with the account given!");
+        }
         return;
     }
 
@@ -112,10 +175,11 @@ public class Menu {
             access = check(sc);
         }
         HashMap<String,Credential> pwds = new HashMap<>();
+        load(pwds);
         boolean run = true;
         while(run) {
             System.out.println("-------Main Menu-------");
-            System.out.println("\n1. Add Password\n2. Search for password\n3. Delete password\n4. Update password\n5. View the all accounts present\n6. Exit");
+            System.out.println("\n1. Add Password\n2. Search for password\n3. Delete password\n4. Update password\n5. View the all accounts present\n6. Exit\n7.Reset password");
             System.out.println("Enter your choice: ");
             int choice = sc.nextInt();
             sc.nextLine();
@@ -123,15 +187,18 @@ public class Menu {
             switch (choice) {
                 case 1:
                     add(pwds,sc);
+                    save(pwds);
                     break;
                 case 2:
                     search(pwds,sc);
                     break;
                 case 3:
                     delete(pwds,sc);
+                    save(pwds);
                     break;
                 case 4:
                     update(pwds,sc);
+                    save(pwds);
                     break;
                 case 5: 
                     view(pwds);
@@ -139,6 +206,9 @@ public class Menu {
                 case 6:
                     run=false;
                     System.out.println("------Sayonara------");
+                    break;
+                case 7:
+                    reset(sc);
                     break;
                 default:
                     System.out.println("Invalid Choice!!");
